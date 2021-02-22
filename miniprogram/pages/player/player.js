491dd266f5347f916b2af1069e696f890001dcfb
Tcgogo <1812497229@qq.com>
2021-02-22 23:56:27 +0800

    '播放器页面布局'

--- miniprogram/pages/player/player.js
+++ miniprogram/pages/player/player.js
@@ -1,18 +1,45 @@
 // pages/player/player.js
+let musiclist = []
+// 正在播放歌曲的index
+let nowPlayingIndex = 0
 Page({
 
   /**
    * 页面的初始数据
    */
   data: {
-
+    picUrl: '',
+    isPlaying: false, // false表示不播放，true表示正在播放
+    isLyricShow: false, //表示当前歌词是否显示
+    lyric: '',
+    isSame: false, // 表示是否为同一首歌
   },
 
   /**
    * 生命周期函数--监听页面加载
    */
   onLoad: function (options) {
+    console.log(options)
+    nowPlayingIndex = options.index
+    musiclist = wx.getStorageSync('musiclist')
+    this._loadMusicDetail(options.musicId)
+  },
+  _loadMusicDetail(musicId) {
+    let music = musiclist[nowPlayingIndex]
+    console.log(music)
+    wx.setNavigationBarTitle({
+      title: music.name,
+    })
 
+    this.setData({
+      picUrl: music.al.picUrl,
+      isPlaying: false,
+    })
+
+    wx.showLoading({
+      title: '歌曲加载中',
+    })
+      wx.hideLoading()
   },
 
   /**
